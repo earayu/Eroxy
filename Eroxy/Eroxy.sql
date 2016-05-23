b5371bfca1a45bd09ec695fbe2b8e4ac793a03b1
earayu <earayu@163.com>
2016-05-23 15:11:35 +0800

    table proxy

---
+++ Eroxy/Eroxy.sql
@@ -0,0 +1,10 @@
+create table proxy(
+ip char(15) primary key not null,
+port char(5) not null,
+delay varchar(255),
+protocal varchar(255),
+type varchar(255),
+location varchar(255),
+inTime datetime,
+life varchar(255)
+);
