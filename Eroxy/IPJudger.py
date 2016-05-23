42b45dc62395fd9c53364239f9d3352c8407b993
earayu <earayu@163.com>
2016-05-23 15:07:52 +0800

    save to mysql

--- Eroxy/IPJudger.py
+++ Eroxy/IPJudger.py
@@ -6,7 +6,7 @@ import time
 #timeout:超时时间   https:默认False时使用http协议,否则使用https协议
 #verify:verify为空时,默认检测IP的网站为httpbin.org/ip. verify可以接受一个网址来覆盖IPJudger的默认行为
 #需要注意的是，网址前不要带协议(直接使用如'www.google.com'形式。)
-def IPJudger(proxy, timeout=10, https=False, verify=None):
+def HTTPJudger(proxy, timeout=10, https=False, verify=None):
     protocal = 'http'
     if https:
         protocal = 'https'
@@ -30,6 +30,12 @@ def IPJudger(proxy, timeout=10, https=False, verify=None):
         return None
     except:
         return None
+
+
+#TODO   通用的IP验证函数，能验证各种协议   HTTP,HTTPS,SOCK5
+def ProxyJudger(proxy, timeout=10, protocal='HTTP', verify=None):
+    _proxy = {protocal: proxy}
+
 
 
 
