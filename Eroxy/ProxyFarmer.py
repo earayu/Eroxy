6c38c15d5a5af0502ede54fb9e0f80e8cb7b35c4
earayu <earayu@163.com>
2016-05-23 20:47:00 +0800

    fix

--- Eroxy/ProxyFarmer.py
+++ Eroxy/ProxyFarmer.py
@@ -7,7 +7,6 @@ import queue
 import time
 import pymysql
 
-
 # 各个线程过滤完IP后都存在这里，按照delay排序
 que = queue.PriorityQueue()
 
@@ -17,13 +16,11 @@ my_headers = \
         "Accept-Encoding": "gzip, deflate, sdch",
         "Accept-Language": "zh-CN,zh;q=0.8",
         "Cache-Control": "max-age=0",
-        "Cookie": "bdshare_ty=0x18; Hm_lvt_f8bdd88d72441a9ad0f8c82db3113a84=1463983094,1463985523; Hm_lpvt_f8bdd88d72441a9ad0f8c82db3113a84=1463985523",
         "Connection": "keep-alive",
-        "Host": "www.youdaili.net",
-        "Referer": "http://www.youdaili.net/Daili/http/4456.html",
         "Upgrade-Insecure-Requests": "1",
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36",
     }
+
 
 # TODO 需要加入header, proxies, data, cookies 甚至auth 等字段。
 # TODO Proxy类可以不填完整，只要有IP和PORT就行了。有了这些可以去IP库查询归属地，运营商等信息
@@ -38,10 +35,37 @@ class ProxyFarmer:
         self.__inTime_rule = None
         self.__protocol_rule = None
         self.__life_rule = None
+        self.__headers = None
+        self.__cookies = None
+        self.__proxies = None
 
-    #IP必须设定规则，因为需要提供一个页面抓取的Proxy计数。
-    #TODO 这儿rule要提供6个正则表达式太麻烦了，也许可以建立一个rules对象来管理。
-    #TODO 而且ProxyFarmer对正则表达式的准确度要求较高。不准确的正则表达式会引发不确定的错误。
+    @property
+    def headers(self):
+        return self.__headers
+
+    @headers.setter
+    def headers(self, headers):
+        self.__headers = headers
+
+    @property
+    def cookies(self):
+        return self.__cookies
+
+    @cookies.setter
+    def cookies(self, cookies):
+        self.__cookies = cookies
+
+    @property
+    def proxies(self):
+        return self.__proxies
+
+    @proxies.setter
+    def proxies(self, proxies):
+        self.__proxies = proxies
+
+    # IP必须设定规则，因为需要提供一个页面抓取的Proxy计数。
+    # TODO 这儿rule要提供6个正则表达式太麻烦了，也许可以建立一个rules对象来管理。
+    # TODO 而且ProxyFarmer对正则表达式的准确度要求较高。不准确的正则表达式会引发不确定的错误。
     def rules(self, ip_rule, port_rule=None, type_rule=None, location_rule=None, protocol_rule=None, life_rule=None):
         self.__ip_rule = ip_rule
         self.__port_rule = port_rule
@@ -53,7 +77,7 @@ class ProxyFarmer:
     # 收获raw_ip. 这些IP没经过验证，很可能不能使用。
     def harvest(self):
         try:
-            r = requests.get(self.__url, timeout=(10, 30), headers=my_headers)
+            r = requests.get(self.__url, timeout=(10, 30), headers=self.__headers, cookies=self.__cookies, proxies=self.__proxies)
             print(r.status_code)
         except:
             return None
@@ -113,33 +137,26 @@ class ProxyFarmer:
             t.join()
         while not que.empty():
             _proxy = que.get()
-            print(type(_proxy))
             yield _proxy
 
     def hibernate(self):
         gen = self.process()
         for proxy in gen:
-            print(proxy)
             try:
                 save(proxy)
-            except  Exception:
-                print("发生异常")
+            except Exception as e:
+                print(e)
 
 
+# pymysql的connection是非线程安全的
 def save(proxy):
-    conn = pymysql.connect(host='localhost', user='username', passwd='password', db='Eroxy', port=3306,
-                           charset='utf8')
-    cur = conn.cursor()  # 获取一个游标
-    sql = 'insert into proxy (ip,port,delay,location,inTime,protocal)' \
-          + ' VALUES (\'' + proxy.ip + '\',\'' + str(proxy.port) + '\',\'' + str(proxy.delay) + '\',\'' + \
-          proxy.location + '\',\'' + proxy.inTime + '\',\'' + proxy.protocol + '\')'
-    print(sql)
-    cur.execute(sql)
+    conn = pymysql.connect(host='localhost', user='earayu', passwd='qwqwqw', db='Eroxy', port=3306, charset='utf8')
+    cur = conn.cursor()
+    sql = 'insert into proxy (ip,port,delay,inTime) VALUES (%s,%s,%s,%s)'
+    cur.execute(sql, (proxy.ip, proxy.port, proxy.delay, proxy.inTime))
     conn.commit()
     cur.close()  # 关闭游标
     conn.close()  # 释放数据库资源
-
-
 
 
 # 获取形如 2016-05-23 13:01:21 的时间字符串
@@ -151,7 +168,7 @@ def getTime(format="%Y-%m-%d %H:%M:%S"):
 
 # 送出可用IP
 # TODO judger应该可以自定义过滤规则。待实现
-def judger(proxy , timeout=10, https=False, verify=None):
+def judger(proxy, timeout=10, https=False, verify=None):
     t = HTTPJudger(proxy.ip + ':' + proxy.port, timeout, https, verify)
     if t is not None:
         proxy.inTime = getTime()
@@ -160,9 +177,16 @@ def judger(proxy , timeout=10, https=False, verify=None):
 
 
 if __name__ == '__main__':
-    p = ProxyFarmer('http://www.youdaili.net/Daili/http/4456.html')
+    p = ProxyFarmer('http://www.youdaili.net/Daili/http/4435.html')
     p.rules("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", '(?<=\d:)\d{2,5}(?=@)', protocol_rule='(?<=@).*?(?=#)', \
             location_rule='(?<=P#).*?(?=\W)')
-
+    p.headers = my_headers
+    p.proxies = {'http': '117.21.182.110:80'}
     p.hibernate()
+
+    p2 = ProxyFarmer('http://www.idcloak.com/proxylist/free-proxy-ip-list.html')
+    p2.rules("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", '(?<=<td>)\d{2,5}(?=</td>)', protocol_rule='(?<=<td>)https?(?=</td>)',
+             location_rule='(?<=">)[A-Za-z]*?(?=&nbsp;)'
+             )
+    p2.hibernate()
 
