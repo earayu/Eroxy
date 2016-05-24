e45efc50a7e8a90d253743c3742e41e32123625e
earayu <earayu@163.com>
2016-05-25 00:49:28 +0800

    patrol

--- Eroxy/ProxyPatrol.py
+++ Eroxy/ProxyPatrol.py
@@ -1,43 +1,38 @@
-import pymysql
 from Eroxy.ProxyFarmer import getTime
 import threading
 from Eroxy.IPJudger import HTTPJudger
 from Eroxy.utils import execute
+import time
 from Eroxy.utils import select
 
 
 # TODO 不要直接删除，最好放在另一个表中。给第二次机会
-def patrol(ip, port):
+def patrol(ip, port, alive):
     _proxy = ip + ':' + port
     ret = HTTPJudger(_proxy)
     if ret is not None:
-        usql = 'update proxy2 set delay=%s where ip=%s'
+        usql = 'update proxy2 set delay=%s, alive=1 where ip=%s'
         execute(usql, (ret[1], ret[0][:ret[0].index(':')]))
-    else:
+    elif alive == 0:
         dsql = 'delete from proxy2 where ip=%s'
         execute(dsql, (ip, ))
+    elif alive == 1:
+        usql = 'update proxy2 set alive=0 where ip = %s'
+        print(usql % ip)
+        execute(usql, (ip,))
 
 
+def loop(offset=-3600):
+    while True:
+        sql = 'select ip,port,alive from proxy2 where inTime < %s'
+        data = select(sql, getTime(offset=offset))
+        if data is not ():
+            for i in data:
+                t = threading.Thread(target=patrol, args=(i[0], i[1], i[2]))
+                t.start()
+        time.sleep(abs(offset))
 
 
 
 if __name__ == '__main__':
-    # # threads = []
-    # conn = pymysql.connect(host='localhost', user='earayu', passwd='qwqwqw', db='Eroxy', port=3306,
-    #                        charset='utf8')
-    # cur = conn.cursor()  # 获取一个游标
-    # sql = 'select ip,port from proxy2 where inTime < %s'
-    # print(sql)
-    # t = getTime()
-    # cur.execute(sql, (t, ))
-    # data = cur.fetchall()
-    sql = 'select ip,port from proxy2 where inTime < %s'
-    data = select(sql, getTime())
-    if data is not ():
-        for i in data:
-            t = threading.Thread(target=patrol, args=(i[0], i[1]))
-            # threads.append(t)
-            t.start()
-    # conn.commit()
-    # cur.close()  # 关闭游标
-    # conn.close()  # 释放数据库资源
+    loop(-1200)
